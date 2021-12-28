60ca6b9f34107dbec1ce8ebfb46a11ce8ac3b99e
j3nny-zhang <jennyzhanng@gmail.com>
2021-12-27 21:45:35 -0500

    Added text

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using static Score;
+
+public class Timer : MonoBehaviour
+{
+    public float timerCount = 0;
+    Text timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scoreValue != 6)
+        {
+            timerCount += Time.deltaTime;
+            int timerInt = (int)timerCount;
+            timer.text = timerInt.ToString();
+        }
+       
+    }
+}
